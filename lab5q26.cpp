49ef0a3e6fee90cb88e024e77ad78f76cde55b71
HarekrushnaPradhan <42186351+HarekrushnaPradhan@users.noreply.github.com>
2018-09-07 03:04:09 -0400

    Add files via upload

---
+++ lab5q26.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	cout << "hello"<<endl;
+	cout << "Write all the odd number between 1 to 100. \n";
+	//declare
+	int a(1);
+	//write all the odd number
+	//write number bet 1 to 100
+	while (a<=100){
+	cout << a <<endl;
+	a+=2;}
+return 0;
+}
